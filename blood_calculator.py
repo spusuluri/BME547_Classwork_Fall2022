7e8e49569bea512cdd21a3a9cad789858c59fb9e
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-09-05 12:53:06 -0400

    Analyzes HDL value for its characterization

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,5 +12,12 @@ def input_HDL():
     HDL_input = input("Enter the HDL value:")
     return int(HDL_input)
 
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        return "Normal"
+    elif 40 <= HDL_value < 60 :
+        return "Borderline Low"
+    else:
+        return "Low"
 
 interface()
