d1ced94e67eac5c2f2449610bd8a3d9d25be7882
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-09-19 13:01:28 -0400

    Added parametrize and test_check_LDL function for testing

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -6,3 +6,10 @@ def test_check_HDL(HDL_value, expected):
     from blood_calculator import check_HDL
     answer = check_HDL(HDL_value)
     assert answer == expected
+
+@pytest.mark.parametrize("LDL_value, expected",[(200, "Very High"), (185, "High") , (130, "Borderline High"), (100, "Normal")])
+
+def test_check_LDL(LDL_value, expected):
+    from blood_calculator import check_LDL
+    answer = check_LDL(LDL_value)
+    assert answer == expected
