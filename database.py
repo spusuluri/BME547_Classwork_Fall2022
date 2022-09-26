c9a16026dcfdbc7d8a460868d467814dc97e7ac8
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-09-26 12:42:39 -0400

    Convert patient record to dictionary that will be stored in database list

--- database.py
+++ database.py
@@ -1,37 +1,52 @@
-def create_patient_entry(patient_name, patient_id, patient_age):
-    new_patient = [patient_name, patient_id, patient_age, []]
+def create_patient_entry(patient_first_name, patient_last_name,  patient_id, patient_age):
+    new_patient = {
+        'First Name' : patient_first_name,
+        'Last Name' : patient_last_name,
+        'Id' : patient_id,
+        'Age' : patient_age,
+        'Tests' : []
+    }
     return new_patient
+
+
+def get_full_name(patient):
+    return "{} {}".format(patient["First Name"], patient["Last Name"])
 
 
 def main():
     db = []
-    db.append(create_patient_entry("Ann Ables", 1, 30))
-    db.append(create_patient_entry("Bob Boyles", 2, 34))
-    db.append(create_patient_entry("Chris Chou", 3, 25))
-    print(db)
-    print(db[2][2])
-    print_db(db)
-    print(find_patient(db, 2))
+    db.append(create_patient_entry("Ann", "Ables", 1, 30))
+    db.append(create_patient_entry("Bob",  "Boyles", 2, 34))
+    db.append(create_patient_entry("Chris",  "Chou", 3, 25))
+    print_database(db)
     add_test_to_patient(db, 3, "HDL", 100)
-    print(db)
+    print_database(db)
+    print("Patient {} is a {}.".format(get_full_name(db[2]), adult_or_minor(db[2])))
 
 
-def print_db(db):
+def print_database(db):
     for patient in db:
-        print("Name: {}, id: {}, age: {}"
-              .format(patient[0], patient[1], patient[2]))
+        print("Name: {}, Id: {}, age: {}"
+              .format(get_full_name(patient), patient["Id"], patient["Age"]))
 
 
 def find_patient(db, patient_id):
     for patient in db:
-        if patient[1] == patient_id:
+        if patient['Id'] == patient_id:
             return patient
     return False
 
 
 def add_test_to_patient(db, patient_id, test_name, test_value):
     patient = find_patient(db, patient_id)
-    patient[3].append((test_name, test_value))
+    patient['Tests'].append((test_name, test_value))
+
+
+def adult_or_minor(patient):
+    if patient["Age"] >= 18:
+        return "adult"
+    else:
+        return "minor"
 
 
 if __name__ == "__main__":
