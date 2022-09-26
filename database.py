dc28bb4c8c063fa0811b77c010291b4e25faf266
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-09-26 12:55:26 -0400

    Edited file to make db a dictionary and made it PEP-8 style

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
-def create_patient_entry(patient_first_name, patient_last_name,  patient_id, patient_age):
+def create_patient_entry(patient_first_name, patient_last_name,
+                         patient_id, patient_age):
     new_patient = {
-        'First Name' : patient_first_name,
-        'Last Name' : patient_last_name,
-        'Id' : patient_id,
-        'Age' : patient_age,
-        'Tests' : []
+        'First Name': patient_first_name,
+        'Last Name': patient_last_name,
+        'Id': patient_id,
+        'Age': patient_age,
+        'Tests': []
     }
     return new_patient
 
@@ -14,27 +15,27 @@ def get_full_name(patient):
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob",  "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris",  "Chou", 3, 25))
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 1, 30)
+    db[22] = create_patient_entry("Bob",  "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris",  "Chou", 3, 25)
     print_database(db)
     add_test_to_patient(db, 3, "HDL", 100)
     print_database(db)
-    print("Patient {} is a {}.".format(get_full_name(db[2]), adult_or_minor(db[2])))
+    print("Patient {} is a {}."
+          .format(get_full_name(db[22]), adult_or_minor(db[22])))
 
 
 def print_database(db):
-    for patient in db:
+    for patient_key in db:
+        print(patient_key)
         print("Name: {}, Id: {}, age: {}"
-              .format(get_full_name(patient), patient["Id"], patient["Age"]))
+              .format(get_full_name(db[patient_key]),
+                      db[patient_key]["Id"], db[patient_key]["Age"]))
 
 
 def find_patient(db, patient_id):
-    for patient in db:
-        if patient['Id'] == patient_id:
-            return patient
-    return False
+    return db[patient_id]
 
 
 def add_test_to_patient(db, patient_id, test_name, test_value):
