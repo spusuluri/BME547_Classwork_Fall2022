85f0df22a5238e5ae892447fded825ffde32e576
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-11-02 13:15:47 -0400

    work from class today with mongodb

--- db_client.py
+++ db_client.py
@@ -9,6 +9,7 @@ r = requests.post("http://127.0.0.1:5000/new_patient",
                   json=out_data)
 print(r.status_code)
 print(r.text)
+"""
 out_data1 = {
     'id': 2,
     'test_name': 'HDL',
@@ -18,3 +19,4 @@ r1 = requests.post("http://127.0.0.1:5000/add_test",
                    json=out_data1)
 print(r1.status_code)
 print(r1.text)
+"""
