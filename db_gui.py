d7082b7e811c59d2df535ae8dd405183926df3cf
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-11-07 13:00:59 -0500

    created a basic window with tkinter

---
+++ db_gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def main_window():
+    root = tk.Tk()  # Basic window with interface
+
+    root.mainloop()  # Starts interface
+
+
+if __name__ == "__main__":
+    main_window()
+
