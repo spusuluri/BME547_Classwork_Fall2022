4fb83377bc40816fec26c42843c92eb7148437e1
Satya Pusuluri <srisatya.pusuluri@duke.edu>
2022-11-07 13:14:44 -0500

    created basic gui and this is the end of lecture 11/7. Will continue on this branch for 11/9 as well

--- db_gui.py
+++ db_gui.py
@@ -4,6 +4,12 @@ from tkinter import ttk
 
 def main_window():
     root = tk.Tk()  # Basic window with interface
+    root.title("Blood Donor Database")
+    root.geometry("600x300")
+    ttk.Label(root, text="Blood Donor Database").grid(column=0, row=0,
+                                                      columnspan=2)
+    ttk.Label(root, text="Name:").grid(column=0, row=1)
+    ttk.Entry(root, width=50).grid(column=1, row=1)
 
     root.mainloop()  # Starts interface
 
